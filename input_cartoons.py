e59a8d325bb45e3dfa4c4470fb5f54875747ff7a
Mukarram Tahir <mtahir@mit.edu>
2017-12-11 21:30:15 -0500

    initial commit

--- input_cartoons.py
+++ input_cartoons.py
@@ -8,8 +8,19 @@ import matplotlib.pyplot as plt
 
 class Cartoon:
     def __init__(self, class_names, path='cartoon_data'):
+        self.caption_dict = {}
+        with open(path + '/captions.txt', 'r') as f:
+            for line in f:
+                line = line.split()
+                self.caption_dict[line[0]] = " ".join(line[1:])
+
         self.train_data = []
         self.train_label = []
+        self.train_captions = []
+
+        self.color_scheme = {}
+        for i, cn in enumerate(class_names):
+            self.color_scheme[cn] = i + 1
 
         for f in listdir(path):
             if f[:3] in class_names:
@@ -21,6 +32,7 @@ class Cartoon:
 
                 self.train_data.append(image.tolist())
                 self.train_label.append(f[:3])
+                self.train_captions.append(self.caption_dict[f[:-4]])
 
         self.train_data = np.array(self.train_data)
         self.train_label = np.array(self.train_label)
